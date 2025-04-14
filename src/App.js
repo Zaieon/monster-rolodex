9baa02dd3459a3cc6c6511fe6ba05fe3f1d92d69
Zaieon <zoejohn007@gmail.com>
2025-04-14 17:15:50 +0100

    Added Styling to background and app Title. Completed the project.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+    this.handleChange = this.handleChange.bind(this)
   };
 
   componentDidMount() {
@@ -20,15 +21,21 @@ class App extends Component {
     .then(users=>this.setState({monsters:users}))
   }
 
+  handleChange(e) {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
-    
+  
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
+
         <SearchBox 
-        handleChange = {e =>this.setState({searchField: e.target.value})} placeHolder='search monsters' />
-        
+        handleChange = {this.handleChange} placeHolder='search monsters' />
+
         <CardList monsters={filteredMonsters} searchField = {searchField} />
        
     </div>
